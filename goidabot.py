a0ef7c4752348dd25e269ed4fd35da1030179e20
aaa232 <53390160+aaa232@users.noreply.github.com>
2025-07-05 00:51:39 +0300

    Update goidabot.py

--- goidabot.py
+++ goidabot.py
@@ -4,6 +4,7 @@ from openai import OpenAI
 from datetime import datetime
 from Tits import tits
 
+#Cюда токен
 bot = telebot.TeleBot('')
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
